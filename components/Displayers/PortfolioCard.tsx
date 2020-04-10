4b1db3fdb8a9ba59fe2d5b9cfa38a1ce30b700a8
espe-on <eljoe0@googlemail.com>
2020-04-10 13:38:07 +0100

    init of PortfolioCard.tsx

---
+++ components/Displayers/PortfolioCard.tsx
@@ -0,0 +1,22 @@
+﻿import {FunctionComponent} from "react";
+
+interface PortfolioCardProps {
+    title: string
+    subtitle?: string;
+    bodyText: string;
+    imageUrl: string;
+    imageAlt?: string;
+}
+
+const PortfolioCard: FunctionComponent<PortfolioCardProps> = ({title, subtitle, bodyText, imageUrl, imageAlt}) => (
+    <div className="portfolio-card">
+        <div className="text-container">
+            <h3 className="title">{title}</h3>
+            <h4 className="subtitle">{subtitle}</h4>
+            <p className="body-text">{bodyText}</p>
+        </div>
+        <img src={imageUrl} alt={imageAlt}/>
+    </div>
+);
+
+export default PortfolioCard;
