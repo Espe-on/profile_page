5a96d9a7b9c0934665b0b98e76a424c94db84132
espe-on <eljoe0@googlemail.com>
2020-04-11 14:43:03 +0100

    added class names to HTML

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,11 @@ import {NextPage} from 'next';
 import Link from "next/link";
 import React, {FunctionComponent} from "react";
 import PageContainer from "../components/Page/PageContainer/PageContainer";
+import '../pages_styles/index.scss'
 
 const HomeSplashComponent: FunctionComponent = () => (
-    <div>
-        <Link href="/home">
-            <a>Enter Site</a>
-        </Link>
+    <div className="homeSplash">
+        <Link href="/home">Enter Site</Link>
     </div>
 );
 
